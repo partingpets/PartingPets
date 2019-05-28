d1b6c51532a7d4a671ed80532539c9b637636db9
MCrank <marco_mcse@hotmail.com>
2019-05-28 00:30:53 -0500

    DI for connection string, Users Controller GetById

---
+++ PartingPets/Controllers/UsersController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PartingPets.Data;
+using PartingPets.Models;
+
+namespace PartingPets.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        readonly UsersRepository _repo;
+
+        public UsersController(UsersRepository repo)
+        {
+            _repo = repo;
+        }
+
+        // GET: api/User
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/User/5
+        [HttpGet("{id}", Name = "Get")]
+        public User Get(int id)
+        {
+            var selectedUser = _repo.GetUserById(id);
+            return selectedUser;
+        }
+
+        // POST: api/User
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/User/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ PartingPets/Models/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PartingPets.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Zipcode { get; set; }
+        public string Email { get; set; }
+    }
+}
