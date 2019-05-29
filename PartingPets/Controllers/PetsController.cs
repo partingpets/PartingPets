7489c95ecf8a5f07177fc822957eef3a1b723921
Tim Harley <Timothy.D.Harley@gmail.com>
2019-05-29 18:56:24 -0500

    wip

--- PartingPets/Controllers/PetsController.cs
+++ PartingPets/Controllers/PetsController.cs
@@ -14,16 +14,40 @@ namespace PartingPets.Controllers
     public class PetsController : ControllerBase
     {
         readonly PetRepository _petRepository;
+        readonly CreatePetRequestValidator _validator;
 
         public PetsController()
         {
             _petRepository = new PetRepository();
+            _validator = new CreatePetRequestValidator();
         }
 
         [HttpGet]
         public ActionResult GetAllPets()
         {
             return Ok(_petRepository.GetAllPets());
+        }
+
+        [HttpPost]
+        public ActionResult AddPet(CreatePetRequest createRequest)
+        {
+            if (_validator.Validate(createRequest))
+            {
+                return BadRequest(new { error = "pets must have a name, user Id, breed, date of birth, and date of death." });
+            }
+            var newPet = _petRepository.AddPet(createRequest.Name, createRequest.UserId, createRequest.Breed, createRequest.DateOfBirth, createRequest.DateOfDeath);
+
+            return Created($"api/pets/{newPet.Id}", newPet);
+        }
+
+        public class CreatePetRequestValidator
+        {
+            public bool Validate(CreatePetRequest requestToValidate)
+            {
+                return string.IsNullOrEmpty(requestToValidate.Name)
+                        || string.IsNullOrEmpty(requestToValidate.Breed)
+                        || string.IsNullOrEmpty(requestToValidate.DateOfBirth.ToString());
+            }
         }
     }
 }
--- PartingPets/Data/PetRepository.cs
+++ PartingPets/Data/PetRepository.cs
@@ -12,11 +12,28 @@ namespace PartingPets.Data
     {
         const string ConnectionString = "Server=localhost; Database=PartingPets; Trusted_Connection=True;";
 
+        public Pet AddPet(string name, int userId, string breed, DateTime dateOfBirth, DateTime dateOfDeath)
+        {
+            using (var connection = new SqlConnection(ConnectionString))
+            {
+                var newPet = connection.QueryFirstOrDefault<Pet>(@"Insert into pets (name, userId, breed, dateOfBirth, dateOfDeath)
+                                                        Output inserted.*
+                                                        Values(@name,@userId,@breed,@dateOfBirth,@dateOfDeath)",
+                    new { name, userId, breed, dateOfBirth, dateOfDeath });
+                if (newPet != null)
+                {
+                    return newPet;
+                }
+
+                throw new Exception("No Pet Found");
+            }
+        }
+
         public IEnumerable<Pet> GetAllPets()
         {
             using (var db = new SqlConnection(ConnectionString))
             {
-                return db.Query<Pet>("select name,breed from pets");
+                return db.Query<Pet>("select * from pets");
             }
         }
     }
