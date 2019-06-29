45b71f7593c105564f4b53c2e4550812bfa40c92
Colin White <clwhite814@gmail.com>
2019-06-29 11:39:24 -0500

    wip

---
+++ PartingPets/Controllers/OrdersController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PartingPets.Data;
+using PartingPets.Models;
+
+namespace PartingPets.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrdersController : ControllerBase
+    {
+        readonly OrdersRepository _repo;
+
+        public OrdersController(OrdersRepository repo)
+        {
+            _repo = repo;
+        }
+
+        // GET: api/Orders
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Orders/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Orders
+        [HttpPost]
+        public ActionResult CreateOrder()
+        {
+            var newOrder = _repo.CreateOrder();
+
+            return Created();
+        }
+
+        // PUT: api/Orders/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ PartingPets/Data/OrdersRepository.cs
@@ -0,0 +1,24 @@
+﻿using Dapper;
+using PartingPets.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PartingPets.Data
+{
+    public class OrdersRepository
+    {
+        const string ConnectionString = "Server=localhost;Database=PartingPets;Trusted_Connection=True;";
+
+        public Orders CreateOrder(Orders newOrderObj)
+        {
+            using (var db = new SqlConnection(ConnectionString))
+            {
+                var newOrder = db.QueryFirstOrDefault<Orders>(@"");
+                return;
+            }
+        }
+    }
+}
