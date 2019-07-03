30689956bf956e1991756e28171056daaebb1188
JonathanPMohan <jonathanmohan.net@gmail.com>
2019-07-03 18:22:41 -0500

    fixed user controller

--- PartingPets/Controllers/UsersController.cs
+++ PartingPets/Controllers/UsersController.cs
@@ -78,7 +78,8 @@ namespace PartingPets.Controllers
         public ActionResult<EditUserRequest> UpdateUser(int id, [FromBody] EditUserRequest updatedUserObj)
         {
             var jwtFirebaseId = UserId;
-            if(updatedUserObj.FireBaseUid != jwtFirebaseId && updatedUserObj.IsAdmin == false)
+            User submittingUser = _repo.GetUserById(jwtFirebaseId);
+            if(updatedUserObj.FireBaseUid != jwtFirebaseId && submittingUser.IsAdmin == false)
             {
                 // return 401 as the User they are passing in is not the same as the one making the request
                 return Unauthorized();
