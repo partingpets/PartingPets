84113adc17e6c6b966ef61ab2b9f64ee4a1c46f9
Colin White <clwhite814@gmail.com>
2019-07-02 19:43:06 -0500

    API GET endpoint is pulling back one order with only one prodcut

--- PartingPets/Models/userOrder.cs
+++ PartingPets/Models/userOrder.cs
@@ -7,5 +7,18 @@ namespace PartingPets.Models
 {
     public class userOrder
     {
+        public int Id { get; set; }
+        public DateTime PurchaseDate { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Street1 { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string ZipCode { get; set; }
+        public string Email { get; set; }
+        public int Quantity { get; set; }
+        public string Name { get; set; }
+        public decimal UnitPrice { get; set; }
+
     }
 }
