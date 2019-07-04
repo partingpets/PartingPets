0429f954474dc46f25f26fe52c3ac6d90089caca
Colin White <clwhite814@gmail.com>
2019-07-04 09:46:22 -0500

    GET orders by ID is working

--- PartingPets/Data/OrdersRepository.cs
+++ PartingPets/Data/OrdersRepository.cs
@@ -73,14 +73,18 @@ namespace PartingPets.Data
         {
             using (var db = new SqlConnection(ConnectionString))
             {
-                var userOrderInfo = db.Query<userOrder>("select o.Id, o.PurchaseDate, u.FirstName, u.LastName, u.Street1, u.City, u.State, u.ZipCode, u.Email from Orders o join [User] u on o.UserID = u.Id where o.UserID = @id").ToList();
+                var sql = "select o.Id, o.PurchaseDate, u.FirstName, u.LastName, u.Street1, u.City, u.State, u.ZipCode, u.Email from Orders o join [User] u on o.UserID = u.Id where o.UserID = @id";
 
-                var userOrderLines = db.Query<OrderLines>("select ol.OrdersId, ol.Quantity, p.Name, ol.UnitPrice, p.Id from Orders o join OrdersLine ol on o.Id = ol.OrdersId join Products p on ol.ProductId = p.Id join [User] u on o.UserID = u.Id where o.UserID = 12");
+                
+                var userOrderInfo = db.Query<userOrder>(sql, new { id }).ToList();
+
+                var userOrderLines = db.Query<OrderLines>("select ol.OrdersId, ol.Quantity, p.Name, ol.UnitPrice, p.Id from Orders o join OrdersLine ol on o.Id = ol.OrdersId join Products p on ol.ProductId = p.Id join [User] u on o.UserID = u.Id").ToList();
 
                 foreach (var userInfo in userOrderInfo)
                 {
+                    ;
                     var matchingOrderLines = userOrderLines.Where(x => x.OrdersId == userInfo.Id).ToList();
-                    userOrderInfo.OrderItems = matchingOrderLines;
+                    userInfo.OrderItems = matchingOrderLines;
                 }
 
                 return userOrderInfo;
